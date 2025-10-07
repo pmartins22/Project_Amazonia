4f0777acbd1ed07be46d172582e1304784e96587
pmartins22 <phenriquecostamartins22@gmail.com>
2025-10-07 20:12:33 +0200

    fix(sleep): fix recursive function bug

--- game/actions/sleep.py
+++ game/actions/sleep.py
@@ -3,42 +3,38 @@ from time import sleep
 from utils.utils import Utils
 
 def start_sleep(game_manager):
-    Utils.clear_terminal()
+    while True:
+        Utils.clear_terminal()
 
-    game_manager.print_game_status()
-    game_manager.print_player_status()
-    game_manager.print_player_inventory(under_bar=True)
+        game_manager.print_game_status()
+        game_manager.print_player_status()
+        game_manager.print_player_inventory(under_bar=True)
 
-    Utils.draw_bar(30, "-", corners="*")
-    print("Do you want to sleep now ?")
-    print()
-    print("1 : Yes")
-    print("2 : Not yet !")
-    Utils.draw_bar(30, "-", corners="*")
-    print()
-    choice = Utils.get_input_int(1, 2, "Enter your choice: ")
+        Utils.draw_bar(30, "-", corners="*")
+        print("Do you want to sleep now ?")
+        print()
+        print("1 : Yes")
+        print("2 : Not yet !")
+        Utils.draw_bar(30, "-", corners="*")
+        print()
+        choice = Utils.get_input_int(1, 2, "Enter your choice: ")
 
-    match choice:
-        case 1:
-            hours = Utils.get_input_int(1, 48, "How many hours do you want to sleep ? ")
+        if choice == 2: break
 
-            estimated_hunger_loss = hours * 0.55
-            min_hunger_threshold = game_manager.player.max_hunger * 0.1
+        hours = Utils.get_input_int(1, 48, "How many hours do you want to sleep: ")
 
-            if game_manager.player.hunger - estimated_hunger_loss < min_hunger_threshold:
-                print("You are to much hunger for sleep this time! Eat first.")
-                sleep(2)
-                start_sleep(game_manager)
+        estimated_hunger_loss = hours * 0.55
+        min_hunger_threshold = game_manager.player.max_hunger * 0.1
 
-            print("You are sleeping...")
+        if game_manager.player.hunger - estimated_hunger_loss < min_hunger_threshold:
+            print("You are to much hunger for sleep this time! Eat first.")
             sleep(2)
+            start_sleep(game_manager)
 
-            hunger_before = game_manager.player.hunger
+        print("You are sleeping...")
+        sleep(2)
 
-            energy_gain = hours
-            game_manager.player.sleep(energy_gain)
+        game_manager.player.sleep(hours)
 
-            print(game_manager.pass_time(hours, tax_energy=False))
-            sleep(3)
-        case 2:
-            return
+        print(game_manager.pass_time(hours, tax_energy=False))
+        sleep(3)
